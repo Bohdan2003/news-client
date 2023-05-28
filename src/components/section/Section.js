dcdc9a53e9c773d169f228ed03a8c2e4e348c2c4
bohdan <killin22003@gmail.com>
2023-05-28 18:32:45 +0300

    fix section

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -1,5 +1,4 @@
-import { useCallback, useEffect, useState, memo } from 'react';
-import { checkIsMatchesWidth } from '../../utils/helpers';
+import { useState, memo } from 'react';
 import { useGetNewsSectionQuery } from "../../api/apiSlice";
 
 import { Tile } from '../tile/Tile';
@@ -27,26 +26,26 @@ const MemoTile = memo(({imgURL, date, descr}) =>
 
 const FilterBtns = ({filter, setFilter}) => {
     return (
-        <div className="section__tabs">
+        <div className="section__filter">
             <button 
-                className={`section__filter-btn ${filter === 'news' ? 'section__filter-btn--active' : ''}`}
+                className={`section__filter-btn ${filter === 'tiles' ? 'section__filter-btn--active' : ''}`}
                 onClick={() => {
-                    setFilter('news')
+                    setFilter('tiles')
                 }}
                 >Новини</button>
             <button 
-                className={`section__filter-btn ${filter === 'articles' ? 'section__filter-btn--active' : ''}`}
+                className={`section__filter-btn ${filter === 'list' ? 'section__filter-btn--active' : ''}`}
                 onClick={() => {
-                    setFilter('articles')
+                    setFilter('list')
                 }}
                 >Статті</button>
         </div>
     )
 }
 
-const Tiles = ({news}) => {
+const Tiles = ({filter, news}) => {
     return (
-        <div className="section__content-box">
+        <div className={`section__content-box ${filter !== "tiles" ? "hidden" : ""}`}>
             <ul className="section__items">
                 {
                     news.map(({_id, ...props}) => 
@@ -61,15 +60,15 @@ const Tiles = ({news}) => {
     )
 }
 
-const List = ({newsList}) => {
+const List = ({filter, news}) => {
     return (
-        <div className="list">
+        <div className={`list ${filter !== "list" ? "hidden" : ""}`}>
             <div className="list__title">
                 Новини розділу
             </div>
             <ul className="list__items">
               {
-                newsList.map(({_id, ...props}) => 
+                news.map(({_id, ...props}) => 
                     <MemoListItem {...props} key={_id}/>
                 )
               }
@@ -83,48 +82,15 @@ const List = ({newsList}) => {
 
 export const Section = ({id}) => {
 
-    const [ filter, setFilter] = useState(null);
+    const [ filter, setFilter] = useState("tiles");
     const { 
             data: news = {newsTile:[], newsList:[]}, 
             isLoading,
             isError
         } = useGetNewsSectionQuery(id)
 
-    const compare = useCallback(() => {
-        checkIsMatchesWidth(800)
-        ? setFilter('news')
-        : setFilter('all')
-    }, [])
-
-    useEffect(() => {
-        compare()
-    }, [])
-
-    useEffect(() => {
-        window.addEventListener('resize', compare);
-        return () => {
-            window.removeEventListener('resize', compare)
-        }
-    }, [])
-
-    const setContent = () => {
-        switch(filter){
-            case 'all':
-                return (<>
-                    <Tiles news={news.newsTile}/>
-                    <List newsList={news.newsList}/>
-                </>)
-            case 'news': 
-                return <List newsList={news.newsList}/>
-            case 'articles':            
-                return <Tiles news={news.newsTile}/>
-        }
-    }
-
     if(isError) return <section className="section"><Error/></section>
     if(isLoading) return <SkeletonSection/>
-
-    const content = setContent()
 
     return (
         <section className="section">          
@@ -136,13 +102,10 @@ export const Section = ({id}) => {
                     Всі новини розділу
                 </a>
             </div>    
-                {
-                    filter !== 'all' 
-                        ? <FilterBtns filter={filter} setFilter={setFilter}/> 
-                        : null
-                }
+            <FilterBtns filter={filter} setFilter={setFilter}/>
             <div className="section__content">
-                {content}
+                <Tiles filter={filter} news={news.newsTile}/>
+                <List filter={filter} news={news.newsList}/>
             </div>       
         </section>
     )
--- src/keys/index.js
+++ src/keys/index.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = 'https://pacific-savannah-21985.herokuapp.com'
-export const IMAGE_URL = `${BACKEND_URL}/images/`
-export const AVATAR_URL = `${BACKEND_URL}/avatars/`
+export const BACKEND_URL = 'https://pacific-savannah-21985.herokuapp.com/'
+export const IMAGE_URL = `${BACKEND_URL}images/`
+export const AVATAR_URL = `${BACKEND_URL}avatars/`
